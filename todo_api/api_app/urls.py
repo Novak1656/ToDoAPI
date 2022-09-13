bb76fb7e035a27f3b0b0fbed0aeffb217ce066e4
acclrtrrr <andrey.novikov2001@yandex.ru>
2022-09-13 20:33:32 +0300

    Добавлен роутер

--- todo_api/api_app/urls.py
+++ todo_api/api_app/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TaskAPIViewSet, UserCreateDeleteAPIView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tasks', TaskAPIViewSet, basename='tasks')
+
 
 urlpatterns = [
-    path('task/my_tasks/', TaskAPIViewSet.as_view({'get': 'my_tasks'}), name='task_list'),
-    path('task/create/', TaskAPIViewSet.as_view({'post': 'create'}), name='task_create'),
-    path('task/update/<int:pk>/', TaskAPIViewSet.as_view({'put': 'update'}), name='task_update'),
-    path('task/delete/<int:pk>/', TaskAPIViewSet.as_view({'delete': 'destroy'}), name='task_delete'),
+    path('', include(router.urls)),
 
     path('user/create/', UserCreateDeleteAPIView.as_view(), name='user_create'),
     path('user/delete/<int:pk>/', UserCreateDeleteAPIView.as_view(), name='user_delete'),
